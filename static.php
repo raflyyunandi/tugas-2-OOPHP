266228f320b0d0cc3d5d580ecfc45b09dadbb4cf
Rafly Yunandi Aliansyah <raflyyunandia@mail.unpas.ac.id>
2019-09-30 21:47:38 +0700

    Membuat contoh static keyword yang dengan OO

--- static.php
+++ static.php
@@ -1,16 +1,31 @@
 <?php 
-class ContohStatic {
-	public static $angka = 1;
+// class ContohStatic {
+// 	public static $angka = 1;
 
-	public static function halo() {
-		return "Halo " . self::$angka++ . " kali";
+// 	public static function halo() {
+// 		return "Halo " . self::$angka++ . " kali";
+
+// 	}
+// }
+
+// echo ContohStatic::$angka;
+// echo "<br>";
+// echo ContohStatic::halo();
+// echo "<hr>";
+// echo ContohStatic::halo();
+
+class ContohStatic2 {
+	public $angka = 1;
+
+	public function halo(){
+		return "halo".$this->angka++ . " kali. <br>";
 
 	}
+ 
 }
+$obj = new ContohStatic2;
+echo $obj->halo();
+echo $obj->halo();
+echo $obj->halo();
 
-echo ContohStatic::$angka;
-echo "<br>";
-echo ContohStatic::halo();
-echo "<hr>";
-echo ContohStatic::halo();
  ?>
