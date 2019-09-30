f33b5d10ba6daff04601b793af1dd319af7e4ebc
Rafly Yunandi Aliansyah <raflyyunandia@mail.unpas.ac.id>
2019-09-30 19:20:41 +0700

    Menambahkan constructor untuk kelas produk

--- produk.php
+++ produk.php
@@ -9,6 +9,13 @@ class Produk {
 			$penerbit = "penerbit",
 			$harga = 0 ;
 
+	public function __construct($judul, $penulis, $penerbit, $harga){
+			$this->judul = $judul;
+			$this->penulis = $penulis;
+			$this->penerbit = $penerbit;
+			$this->harga = $harga;
+	}		
+
 	public function getLabel(){
 		return "$this->penulis, $this->penerbit, $this->harga";
 	}	
@@ -22,17 +29,20 @@ class Produk {
 // $produk1 -> judul = "Naruto";
 // var_dump($produk1);
 
-$produk3 = new Produk();
-$produk3 -> judul = "Tokyo Ghoul";
-$produk3 -> penulis = "Ishida Sui";
-$produk3 -> penerbit = "Shonen Jump";
-$produk3 -> harga = 90000;
+// $produk3 = new Produk();
+// $produk3 -> judul = "Tokyo Ghoul";
+// $produk3 -> penulis = "Ishida Sui";
+// $produk3 -> penerbit = "Shonen Jump";
+// $produk3 -> harga = 90000;
 
-$produk4 = new Produk();
-$produk4 -> judul = "Uncharted";
-$produk4 -> penulis = "Neil Druckman";
-$produk4 -> penerbit = "Sony Computer";
-$produk4 -> harga = 75000;
+$produk3 = new Produk ("Naruto", "Masashi Kisimoto", "Shounen Jump", 30000);
+$produk4 = new Produk ("Uncharted", "Neil Druckman", "Sony Computer", 75000);
+
+// $produk4 = new Produk();
+// $produk4 -> judul = "Uncharted";
+// $produk4 -> penulis = "Neil Druckman";
+// $produk4 -> penerbit = "Sony Computer";
+// $produk4 -> harga = 75000;
 
 
 echo "Komik : " . $produk3 -> getLabel();
