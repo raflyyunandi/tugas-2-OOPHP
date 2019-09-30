814fdf6d7404c5695cb7490a92d7267c49633e00
Rafly Yunandi Aliansyah <raflyyunandia@mail.unpas.ac.id>
2019-09-30 19:04:50 +0700

    memanggil property menggunakan echo

--- produk.php
+++ produk.php
@@ -10,8 +10,16 @@ class Produk {
 			$harga = 0 ;
 }
 
-$produk1 = new Produk();
-$produk1 -> judul = "Naruto";
-var_dump($produk1);
+// $produk1 = new Produk();
+// $produk1 -> judul = "Naruto";
+// var_dump($produk1);
+
+$produk3 = new Produk();
+$produk3 -> judul = "Tokyo Ghoul";
+$produk3 -> penulis = "Ishida Sui";
+$produk3 -> penerbit = "Shonen Jump";
+$produk3 -> harga = 90000;
+
+echo "Komik : $produk3->penulis, $produk3->penerbit ";
 
  ?>
