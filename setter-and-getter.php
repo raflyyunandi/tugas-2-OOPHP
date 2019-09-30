fe81f680daeb7a4e2cffd36b8c6d57287d37a7c5
Rafly Yunandi Aliansyah <raflyyunandia@mail.unpas.ac.id>
2019-09-30 21:38:19 +0700

    menambahkan semua setter dan getter untuk setiap properti private kemudian memanggilnya

--- setter-and-getter.php
+++ setter-and-getter.php
@@ -8,13 +8,14 @@ class Produk {
 	// 		$penulis = "penulis",
 	// 		$penerbit = "penerbit",
 	// 		$harga = 0 ;
-	public 	$judul ,
-			$penulis ,
-			$penerbit;
+	private 	$judul ,
+				$penulis ,
+				$harga ,
+				$penerbit;
 
 	
-	protected $diskon;
-	private $harga;		
+	protected 	$diskon;
+			
 			// $jmlHal ,
 			// $lamaMain; 
 			// $tipe;
@@ -30,6 +31,33 @@ class Produk {
 			// $this->tipe = $tipe;
 	}		
 
+	public function setJudul ($judul) {
+			$this->judul = $judul;
+	}
+
+	public function getJudul () {
+			return $this->judul;
+	}
+
+	public function setPenerbit ($penerbit) {
+			$this->penerbit = $penerbit;
+	}
+
+	public function getPenerbit () {
+			return $this->penerbit;
+	}
+
+		public function setPenulis ($penulis) {
+			$this->penulis = $penulis;
+	}
+
+	public function getPenulis () {
+			return $this->penulis;
+	}
+
+	public function setHarga($harga) {
+		$this->harga = $harga;
+	}
 
 	public function getHarga(){
 		return $this->harga - ($this->harga * $this->diskon / 100);
@@ -75,6 +103,10 @@ class Komik extends Produk {
 
 	public function setDiskon ($diskon) {
 		$this->diskon = $diskon;
+	}
+
+	public function getDiskon () {
+		return $this->diskon;
 	}
 
 
@@ -144,4 +176,7 @@ echo "<hr>";
 
 echo $produk3->setDiskon(10);
 echo $produk3->getHarga();
+echo "<hr>";
+
+echo $produk3-> getPenulis();
  ?>
